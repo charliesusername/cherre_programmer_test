ed0f7ba226be3f14dbfc43c11115372439863742
iamcharliecohen <iamcharliecohen@gmail.com>
2019-08-26 13:58:11 -0400

    Uploading my Solution

---
+++ CCohen_Solution.sql
@@ -0,0 +1,16 @@
+SELECT id, first_name, last_name FROM people
+WHERE id in (
+  SELECT personId FROM visits
+  GROUP BY personID
+  ORDER BY count(personID) DESC
+  LIMIT 10)
+;
+
+INSERT INTO frequent_browsers
+SELECT personId as person_id, COUNT(personId) as num_sites_visited
+FROM visits
+GROUP BY personId
+ORDER BY COUNT(personId) DESC
+LIMIT 10;
+
+
